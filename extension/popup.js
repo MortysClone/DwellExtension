c71eac4816f8d78782dda0a166f9c3dfe0c12b3f
kissesy <b7712983@naver.com>
2020-11-03 15:14:23 +0900

    your code

--- extension/popup.js
+++ extension/popup.js
@@ -2,6 +2,11 @@ var port = chrome.extension.connect({
     name: "Sample Communication"
 });
 port.postMessage("Hi BackGround");
+
+/*
+msg : array  
+ex) msg = ['url', 'url', 'url']
+*/
 port.onMessage.addListener(function(msg) {
     const ul = document.querySelector("#list");
     console.log(typeof(msg));
